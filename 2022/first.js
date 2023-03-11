1c084b39eacc400981c791adc82a4813540de562
Klara Sejnova <klarasejnova@mezzanine.local>
2023-03-11 22:43:35 +0100

    first part ready

--- 2022/first.js
+++ 2022/first.js
@@ -2,36 +2,34 @@ const fs = require('fs');
 const content = fs.readFileSync('./calories.txt', 'utf8');
 const arr = content.split(/\r?\n/);
 
-var arr2 = []
-var indexes = []
 function my_function(pole){
+    var indexes = []
     for (var i = 0; i < pole.length; i++) {   
         indexes.push(arr.indexOf('',i))
-
-    // if (pole[i] == '') {
-    //     return   
-    // }
-    // else {
-    //     arr2.push(Number(arr[i]))
-    // }
-}
-}
-function spliByIndex(index){
-    for (var i=0; i<= index.length; i++) {
-        console.log(arr.slice(index[i-1],index[i]));
     }
+    return indexes
 }
 
-let sum = 0
-function soucet(array){
-    for (let i = 0; i < array.length; i++) {
-        sum += array[i];
-      }
-      return sum
+function spliByIndex(index){
+    var arrays = []
+    var numberArray = []
+    for (var i=0; i<= index.length; i++) {
+        arrays.push(arr.slice(index[i-1],index[i]));
+    }
+    for (var i = 0; i < arrays.length; i++){ 
+        for (var j =0; j < arrays[i].length; j++) {
+            if ( arrays[i][j] == '') {   
+            arrays[i].splice(j, 1); 
+            }
+        }
+    numberArray.push(arrays[i].map(item=>Number(item)))
+    }
+    return numberArray 
 }
-my_function(arr)
-soucet(arr2)
+
+
+var indexes = my_function(arr)
 var newIndexes = [...new Set(indexes)]
-spliByIndex(newIndexes)
-var newIndexes = [...new Set(indexes)]
-console.log(arr2, sum, newIndexes)
+var numberArray = spliByIndex(newIndexes)
+var soucetSum = numberArray.map(arr => arr.reduce((sum,item) => sum += item, 0))
+console.log(soucetSum.reduce((a,b,i) => a[0] < b ? [b,i] : a, [Number.MIN_VALUE,-1]))
