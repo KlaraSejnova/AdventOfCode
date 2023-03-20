06633b0ff83e8056b9836893c0eab771c66f047a
Klara Sejnova <klarasejnova@mezzanine.local>
2023-03-20 11:54:33 +0100

    task 2 part 2

--- 2022/second.js
+++ 2022/second.js
@@ -30,10 +30,38 @@ for (var i = 0; i < arr.length; i++){
     if (arr[i] == 'C Z') {
         results.push(Number(3 + 3))
     }
+    // second part of the task number Two:
+    
+    // if (arr[i] == 'A X') {
+    //     results.push(Number(3 + 0))
+    // }
+    // if (arr[i] == 'B X') {
+    //     results.push(Number(1 + 0))
+    // }
+    // if (arr[i] == 'C X') {
+    //     results.push(Number(2 + 0))
+    // }
+    // if (arr[i] == 'A Y') {
+    //     results.push(Number(1 + 3))
+    // }
+    // if (arr[i] == 'B Y') {
+    //     results.push(Number(2 + 3))
+    // }
+    // if (arr[i] == 'C Y') {
+    //     results.push(Number(3 + 3))
+    // }
+    // if (arr[i] == 'A Z') {
+    //     results.push(Number(2 + 6))
+    // }
+    // if (arr[i] == 'B Z') {
+    //     results.push(Number(3 + 6))
+    // }
+    // if (arr[i] == 'C Z') {
+    //     results.push(Number(1 + 6))
+    // }
 }
-console.log(results)
 let result = results.reduce((a, b) => {
-    return a + b;
-  });
-  
-  console.log(result);
+  return a + b;
+});
+
+console.log(result);
