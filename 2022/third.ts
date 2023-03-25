e228428bc75385a3112235d76f642f08efbda168
Klara Sejnova <klarasejnova@mezzanine.local>
2023-03-25 21:04:03 +0100

    second part of third task added

--- 2022/third.ts
+++ 2022/third.ts
@@ -56,14 +56,33 @@ const smallLetters: string[] = [
   "Z",
 ];
 const batohy: string[] = [];
+let group: string[][] =[];
 for (var i: number = 0; i < arr.length; i++) {
   const rucksacks: string[] = arr[i].split("");
+  group.push(rucksacks)
   const middleIndex: number = Math.ceil(rucksacks.length / 2);
-  const firstHalf: string[] = rucksacks.splice(0, middleIndex);
-  const secondHalf: string[] = rucksacks.splice(-middleIndex);
+  const firstHalf: string[] = rucksacks.slice(0, middleIndex);
+  const secondHalf: string[] = rucksacks.slice(-middleIndex);
   batohy.push(findCommonElements3(firstHalf, secondHalf));
+  
 }
-console.log(batohy);
+let arrItem: string[] = []
+let sameItem: string = 'string'
+while (group.length > 1){
+    let firstThree: string[][] = group.slice().splice(0, 3);
+    for (var i = 0; i < firstThree[0].length; i++){
+        for (var j = 0; j < firstThree[1].length; j++){
+            for (var k= 0; k < firstThree[2].length; k++){
+                if (firstThree[0][i] == firstThree[1][j] && firstThree[2][k] == firstThree[1][j] ){
+                     sameItem = firstThree[2][k]
+                }      
+            }
+        }
+    }
+    arrItem.push(sameItem)
+    group.splice(0,3)
+}
+// console.log(arrItem)
 function findCommonElements3(arr1: string[], arr2: string[]): string {
   return arr1.filter((element) => arr2.includes(element))[0];
 }
@@ -74,5 +93,15 @@ for (var j: number = 0; j < batohy.length; j++)
       indexes.push(i + 1);
     }
   }
+
+  const indexes2: number[] = [];
+for (var j: number = 0; j < batohy.length; j++)
+  for (var i: number = 0; i < smallLetters.length; i++) {
+    if (arrItem[j] == smallLetters[i]) {
+      indexes2.push(i + 1);
+    }
+  }
 let sum = indexes.reduce((a, b) => a + b);
-console.log(sum);
+let sum2 = indexes2.reduce((a, b) => a + b);
+
+console.log(sum2)
